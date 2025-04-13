673df165603feaa21c7c3f142e6a5a0768bb8da1
syskaseb <syska.seb@gmail.com>
2025-04-13 14:35:03 +0200

    feat: separate server side and client side address for the API

--- src/api-client/core/OpenAPI.ts
+++ src/api-client/core/OpenAPI.ts
@@ -19,8 +19,15 @@ export type OpenAPIConfig = {
     ENCODE_PATH?: ((path: string) => string) | undefined;
 };
 
+const isServer = typeof window === "undefined";
+
+const DEFAULT_API_BASE = "http://localhost:8077";
+const DEFAULT_NEXT_PUBLIC_API_BASE = "http://localhost:8077";
+
 export const OpenAPI: OpenAPIConfig = {
-    BASE: process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:8077',
+    BASE: isServer
+        ? process.env.API_BASE_URL ?? DEFAULT_API_BASE
+        : process.env.NEXT_PUBLIC_API_BASE ?? DEFAULT_NEXT_PUBLIC_API_BASE,
     VERSION: '0.0.1-SNAPSHOT',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
--- src/app/applicants/[id]/page.tsx
+++ src/app/applicants/[id]/page.tsx
@@ -1,3 +1,4 @@
+// app/applicants/[id]/page.tsx
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import { getApplicantsFlat } from "@/api-client/customApplicantClient";
